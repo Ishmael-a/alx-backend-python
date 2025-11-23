d48121c7d07b6996b2030d7d992ce2d70ead11a7
Ishmael-a <abuishmaelyusif204@gmail.com>
2025-11-23 18:34:47 +0000

    Refactor URL routing to use drf_routers for consistency and add NestedFilterBackend to REST framework settings

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework import routers as drf_routers
 from .views import ConversationViewSet, MessageViewSet
+from rest_framework_nested import routers
 
-router = routers.DefaultRouter()
+router = drf_routers.DefaultRouter()
 router.register("conversations", ConversationViewSet, basename="conversations")
 router.register("messages", MessageViewSet, basename="messages")
 
+conversation_router = routers.NestedSimpleRouter(router, "conversations", lookup="conversation")
+conversation_router.register("messages", MessageViewSet, basename="conversation-messages")
+
+
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(conversation_router.urls)),
 ]
 
 # urlpatterns = router.urls
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -50,6 +50,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_FILTER_BACKENDS': 'rest_framework_nested_test.filters.NestedFilterBackend',
     'PAGE_SIZE': 10,
 }
 
